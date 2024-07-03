58c87290c77d1c1f5c445b47f9d473cf361f2fca
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-07-03 16:18:13 -0700

    fixed update.sh

--- update.sh
+++ update.sh
@@ -5,3 +5,5 @@ sudo apt-get install cmake build-essential
 cd /unitree/dev/go2_firmware_tools
 git fetch --all && git reset --hard origin/master
 pip install -r requirements.txt
+chmod +x update.sh
+chmod +x start.sh
